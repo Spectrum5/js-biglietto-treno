0283bab256b78c3c6ad0c3b33a38670410ac7537
Spectrum5 <toskusergio@gmail.com>
2022-12-19 13:38:18 +0100

    Inizializzazione progetto

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//Chiedere il numero di km da percorrere 
+
+//Chiedi età del passeggero
+
+//Calcolare il prezzo del biglietto:
+//Prezzo = 0.21€ x km
+    //SE l'utente è minorenne (< 18anni) applicare uno sconto del 20%
+    //SE l'utente ha più di 65 anni (> 65anni) applicare uno sconto del 40%
+
+//Arrotondare il prezzo a massimo 2 decimali
